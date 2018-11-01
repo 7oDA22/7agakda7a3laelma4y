a9d0130b7b25e9473c5264765eb809cb6f5a10b8
7oDA22 <34072752+7oDA22@users.noreply.github.com>
2018-11-01 01:26:19 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,6 +73,8 @@ if (message.content.startsWith(adminprefix + 'setT')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk");
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
+});
+
 
 
 
