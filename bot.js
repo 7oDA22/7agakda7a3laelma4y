9224148ba70bf8409faae372b12a8d75539e0e90
7oDA22 <34072752+7oDA22@users.noreply.github.com>
2018-11-04 23:46:16 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,26 @@ const client = new Discord.Client();
 var prefix = "m"
 
 
+client.on("ready", () => {
+    client.user.setPresence({
+      status: 'dnd',
+      game: { 
+         type: 0,
+         name: 'Surprise Mother Fucker :) ',
+         details: `Mahmoud-QuaStyle`,
+         url: 'http://twitch.tv/Streammingg',
+         state: `إنْ لَمْ تَجِد لكْ حآقدْ إعلمْ أنْك إنسآن فآشِلْ`,
+        application_id: '281376075802476544',
+         assets: {
+            small_image: `366835431037337600`,
+            small_text: ' Take This ! ' ,
+            large_image: `370451271133429760`,
+            large_text: `GAMING` }
+    
+      }
+        });
+    });
+    
 
 client.on('message' , async message => {
             if(message.content.startsWith(prefix + "tbeh")) {
